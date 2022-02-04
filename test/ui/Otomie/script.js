006d14ab07a0ceec70cc89f80c8717bd4b097419
Tree-Takahashi <takahashi.hiroki@realize.tdsi.co.jp>
2022-02-04 11:50:16 +0900

    UI周り、デザインfix版、見た目のみ入れ込み

--- test/ui/Otomie/script.js
+++ test/ui/Otomie/script.js
@@ -1,4 +1,25 @@
 'use strict';
+let isdelete = false;
+console.log(isdelete);
+const call = (tf)=>{
+    isdelete = tf;
+    console.log("tf:  " + tf);
+
+    if(isdelete == true){
+        changeScreen();
+    }else{
+        alert("isdelete = false");
+    }
+}
+
+Delete(call);
+
+const changeScreen = ()=>{
+    //UI切り替え処理。。。
+}
+
+console.log(isdelete);
+
 
 // スプラッシュ画面 - アニメ終わりで非表示して次へ
 const appLogoGrp = document.querySelector('.FadeOutAppLogo');
@@ -33,25 +54,55 @@ document.getElementById("StartCard").onclick = function() {
 };
 
 // 収録準備画面 - 画面クリックで録画ボタン引っ込むor現れる
-const buttonStartRec = document.querySelector('#ButtonStartRec');
+// const buttonStartRec = document.querySelector('#ButtonStartRec');
 
-document.getElementById("RecWindow").onclick = function() {
-    if(buttonStartRec.classList.contains('DownRecBtnAnim') == false){ //初回引っ込み
-        buttonStartRec.classList.add('DownRecBtnAnim');
-        if(buttonStartRec.classList.contains('UpRecBtnAnim') == true){ //2回目以降引っ込み
-            buttonStartRec.classList.remove('UpRecBtnAnim');
-            buttonStartRec.classList.add('DownRecBtnAnim');
-        }
-    }else{ //現れる
-        buttonStartRec.classList.remove('DownRecBtnAnim');
-        buttonStartRec.classList.add('UpRecBtnAnim');
-    }
-};
+// document.getElementById("RecWindow").onclick = function() {
+//     if(buttonStartRec.classList.contains('DownRecBtnAnim') == false){ //初回引っ込み
+//         buttonStartRec.classList.add('DownRecBtnAnim');
+//         if(buttonStartRec.classList.contains('UpRecBtnAnim') == true){ //2回目以降引っ込み
+//             buttonStartRec.classList.remove('UpRecBtnAnim');
+//             buttonStartRec.classList.add('DownRecBtnAnim');
+//         }
+//     }else{ //現れる
+//         buttonStartRec.classList.remove('DownRecBtnAnim');
+//         buttonStartRec.classList.add('UpRecBtnAnim');
+//     }
+// };
 
 // 収録準備画面 - 録画ボタンクリックで収録開始
-document.getElementById("ButtonStartRec").onclick = function(event) {
-    alert('録画開始');
-    event.stopPropagation(); //RecWindowのonclick抑止
-};
-// 停止ボタンに見た目変化
-// メーター動かす
+const buttonStartRec = document.getElementById('ButtonStartRec');
+buttonStartRec.addEventListener('click', recClick);
+
+function recClick(){
+    buttonStartRec.classList.remove('NormalRecBtn');
+    buttonStartRec.classList.add('StartRecBtn');
+}
+
+// 収録準備画面 - 収録アニメ終了でアイコン状態に
+
+
+// テスト用
+document.addEventListener('keypress', keypress_ivent);
+const recContainer = document.getElementById('RecContainer');
+function keypress_ivent(e) {
+    if(e.key === 'a' || e.key === 'A'){
+        //Aキーが押された時の処理 //収録終了
+        recContainer.classList.remove('RecFinish');
+        recContainer.classList.add('RecIcon');
+	}
+    if(e.key === 'b' || e.key === 'B'){
+        //Bキーが押された時の処理 //左下アイコンタップ
+        recContainer.classList.remove('RecIcon');
+        recContainer.classList.add('RecPlayer');
+	}
+    if(e.key === 'c' || e.key === 'C'){
+        //Cキーが押された時の処理 //×アイコンタップ
+        recContainer.classList.remove('RecPlayer');
+        recContainer.classList.add('RecIcon');
+	}
+	return false; 
+}
+
+
+
+
